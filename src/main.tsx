9757eb30d19e7977f8bed03133d196728f53ba2a
Elizaveta S <elizaveta.s.webdev@gmail.com>
2024-09-04 21:39:40 +0300

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { store } from './app/store.ts';
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <Provider store={store}>
-            <BrowserRouter basename="/test-task/">
+            <BrowserRouter basename="/test-deploy/">
                 <App />
             </BrowserRouter>
         </Provider>
